87596f740ff85ea46774fc5201b3ff7e87a345a8
ChloeCDN <chloe@chloedev.de>
2023-11-02 09:24:09 +0100

    Rewrite: - Now applies unfocusing on the elements directly on click/release, instead of running in the render-method. Improves performance, and fixes an issue where elements were automatically unfocused whenever you move your mouse from them, especially annoying on text fields.

---
+++ src/main/java/de/chloedev/optionfocusfix/mixin/MixinParentElement.java
@@ -0,0 +1,32 @@
+package de.chloedev.optionfocusfix.mixin;
+
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.ParentElement;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(ParentElement.class)
+
+public interface MixinParentElement {
+
+    @Shadow
+    List<? extends Element> children();
+
+    @Inject(method = "mouseClicked", at = @At(value = "RETURN", ordinal = 1))
+    default void fixFocus$clicked$1(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        this.children().forEach(element -> element.setFocused(false));
+    }
+
+    @Inject(method = "mouseReleased", at = @At("RETURN"))
+    default void fixFocus$released(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        this.children().forEach(element -> {
+            if (!(element instanceof TextFieldWidget)) element.setFocused(false);
+        });
+    }
+}

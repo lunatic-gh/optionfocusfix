a52ceda461712ad0fa5740ec11c4d5e3698173e5
CommandGenius <rvlcariati@gmail.com>
2024-06-19 19:00:19 -0400

    properly fix bug

--- src/main/java/de/chloedev/optionfocusfix/mixin/MixinParentElement.java
+++ src/main/java/de/chloedev/optionfocusfix/mixin/MixinParentElement.java
@@ -18,7 +18,7 @@ public interface MixinParentElement {
     @Shadow
     List<? extends Element> children();
 
-    @Inject(method = "mouseClicked", at = @At(value = "RETURN", ordinal = 1))
+    @Inject(method = "mouseClicked", at = @At(value = "RETURN", ordinal = 0))
     default void fixFocus$clicked$1(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
         this.children().forEach(element -> element.setFocused(false));
     }
